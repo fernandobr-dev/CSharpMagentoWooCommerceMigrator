4b29b8dfefd6563850a12811df28a0d50f0626d7
luizfernando <luizfernando.dev@gmail.com>
2023-09-30 13:06:50 -0300

    list all atttributes sets in magento

---
+++ DataMagento/AttributeSet.cs
@@ -0,0 +1,8 @@
+﻿namespace CSharpMagentoWooCommerceMigrator.DataMagento;
+
+class AttributeSet
+{
+    public int attribute_set_id {  get; set; }
+    public string attribute_set_name { get; set;}
+
+}
